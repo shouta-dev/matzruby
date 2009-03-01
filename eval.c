c54ab215bc83cb32f447207b7992f6841596691a
Brent Roman <brent@mbari.org>
2009-03-01 13:53:04 -0800

    removed volatile qualifier on parameter for ruby_cleanup(). corrected volatile qualifier on block_pass()'s NODE *, removed unneeded label in eval_opt_n(), removed unneeded result assignment in rb_call0, added missed volatile qualifiers in rb_load()

--- eval.c
+++ eval.c
@@ -221,7 +221,7 @@ static VALUE proc_invoke _((VALUE,VALUE,VALUE,VALUE));
 static VALUE rb_f_binding _((VALUE));
 NOINLINE(static void rb_f_END _((void)));
 static VALUE rb_f_block_given_p _((void));
-static VALUE block_pass _((volatile VALUE, volatile NODE *));
+static VALUE block_pass _((volatile VALUE, NODE *volatile));
 
 VALUE rb_cMethod;
 static VALUE method_call _((int, VALUE*, VALUE));
@@ -1600,12 +1600,13 @@ ruby_finalize()
 }
 
 int
-ruby_cleanup(ex)
-    volatile int ex;
+ruby_cleanup(exArg)
+    int exArg;
 {
     int state;
     volatile VALUE errs[2];
     unsigned nerr;
+    volatile int ex = exArg;
 
     errs[1] = ruby_errinfo;
     ruby_safe_level = 0;
@@ -3003,8 +3004,9 @@ eval_node_volatile(opt_n, void)
   int state;
   PUSH_TAG(PROT_LOOP);
   switch (state = EXEC_TAG()) {
+    case TAG_NEXT:
+      state = 0;
     case 0:
-    opt_n_next:
       while (!NIL_P(rb_gets())) {
 	opt_n_redo:
 	  rb_eval(self, node->nd_body);
@@ -3014,9 +3016,7 @@ eval_node_volatile(opt_n, void)
     case TAG_REDO:
       state = 0;
       goto opt_n_redo;
-    case TAG_NEXT:
-      state = 0;
-      goto opt_n_next;
+
     case TAG_BREAK:
       state = 0;
     default:
@@ -6193,8 +6193,6 @@ rb_call0(klass, recv, id, oid, argc, argv, body, flags)
 		result = prot_tag->retval;
 		state = 0;
 	    }
-            else
-                result = Qnil;
 	    POP_TAG();
 	    if (event_hooks) {
 		EXEC_EVENT_HOOK(RUBY_EVENT_RETURN, ruby_current_node,
@@ -7104,10 +7102,10 @@ rb_load(fname, wrap)
     VALUE tmp;
     int state;
     volatile int prohibit_int = rb_prohibit_interrupt;
-    ID last_func;
+    volatile ID last_func;
     volatile VALUE wrapper = ruby_wrapper;
     VALUE self = ruby_top_self;
-    NODE *last_node;
+    NODE *volatile last_node;
     NODE *volatile saved_cref = ruby_cref;
 
     if (wrap && ruby_safe_level >= 4) {
@@ -9243,7 +9241,7 @@ proc_binding(proc)
 static VALUE
 block_pass(self, node)
     volatile VALUE self;
-    volatile NODE *node;
+    NODE *volatile node;
 {
     volatile VALUE proc = rb_eval(self, node->nd_body);
     VALUE b;
